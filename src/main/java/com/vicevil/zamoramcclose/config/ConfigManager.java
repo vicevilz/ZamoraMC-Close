88b3576704e10253ed67e33da869b39f8ec58257
VicevilZ <120321914+vicevilz@users.noreply.github.com>
2026-07-24 09:25:04 +0200

    Add configuration reload command

--- src/main/java/com/vicevil/zamoramcclose/config/ConfigManager.java
+++ src/main/java/com/vicevil/zamoramcclose/config/ConfigManager.java
@@ -6,15 +6,27 @@ import org.bukkit.plugin.java.JavaPlugin;
 
 public final class ConfigManager {
 
-    private final Configuration configuration;
+    private final JavaPlugin plugin;
+    private Configuration configuration;
 
     public ConfigManager(JavaPlugin plugin) {
+        this.plugin = plugin;
         plugin.saveDefaultConfig();
         this.configuration = plugin.getConfig();
     }
 
     public ConfigManager(Configuration configuration) {
+        this.plugin = null;
         this.configuration = configuration;
+    }
+
+    public void reload() {
+        if (plugin == null) {
+            return;
+        }
+
+        plugin.reloadConfig();
+        configuration = plugin.getConfig();
     }
 
     public String getMessage(String key) {
